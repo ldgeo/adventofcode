ca533ae866f95704effcbbd488cd8b3079cfa9fc
Ludovic Delauné <message@cartodev.com>
2020-12-17 15:08:43 +0100

    day17 optim

--- 2020/day17.py
+++ 2020/day17.py
@@ -4,9 +4,9 @@ result dim4 : 2236
 
 times:
 
-dim=3 (python3.9): 1s
-dim=4 (python3.9): 22s
-dim=4 (pypy3): 16s
+dim=3 (python3.9): 0.4s
+dim=4 (python3.9): 19s
+dim=4 (pypy3): 10s
 
 """
 
@@ -14,6 +14,7 @@ from itertools import product
 
 dimensions = 4
 cycles = 6
+root_coords = (0,) * dimensions
 
 with open('day17.input') as f:
     actives = {
@@ -25,21 +26,28 @@ with open('day17.input') as f:
 
 
 def neighbors(*coords):
+    """
     for shift in product((1, 0, -1), repeat=dimensions):
         if shift == (0,) * dimensions:
             continue
         yield tuple([c + s for c, s in zip(coords, shift)])
+    """
+    return {
+        tuple([c + s for c, s in zip(coords, shift)])
+        for shift in product((1, 0, -1), repeat=dimensions)
+        if shift != root_coords
+    }
 
 
 newactives = actives.copy()
 
 for _ in range(cycles):
     for active in actives:
-        potes = set(neighbors(*active))
+        potes = neighbors(*active)
         if len(potes & actives) not in (2, 3):
             newactives.remove(active)
         for pote in potes:
-            if len(set(neighbors(*pote)) & actives) == 3:
+            if len(neighbors(*pote) & actives) == 3:
                 newactives.add(pote)
 
     actives = newactives.copy()
