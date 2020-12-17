c299f3d9a0f1b28a26158b33b8b58e34299cf569
Ludovic Delauné <message@cartodev.com>
2020-12-17 10:02:12 +0100

    add time execution

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,3 +1,11 @@
+"""
+Van Eck's sequence
+
+times :
+
+python3.9: 8.9s
+pypy3    : 0.7s
+"""
 
 startseq = [9,19,1,6,0,5,4]
 # iterations = 2020
@@ -5,6 +13,7 @@ iterations = 30_000_000
 
 # array of last positions
 pos = [0] * iterations
+
 for idx, v in enumerate(startseq[:-1], start=1):
     pos[v] = idx
 
