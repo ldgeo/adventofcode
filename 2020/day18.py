8a798d8d83aa3525e02c87a134da7d6185d8bf60
Ludovic Delauné <message@cartodev.com>
2020-12-18 17:15:07 +0100

    day18: add part2 generalization

--- 2020/day18.py
+++ 2020/day18.py
@@ -1,5 +1,11 @@
 """
 answer part1 : 25190263477788
+answer part2 : 297139939002972
+
+times:
+
+python3.9: part1: 0.37s
+python3.9: part2: 0.58s
 """
 import operator
 import re
@@ -11,9 +17,13 @@ ops = {
 
 pattern = re.compile(r'\(([\d\s\+\*]+)\)')
 
+# precedence_op = None
+# part 2
+precedence_op = re.compile(r'(\d+\s+\+\s+\d+)')
+
 
 def reduce(it, initializer=None):
-    """Custom reduce fuction handling 3 terms
+    """Recursive reduce with 3 terms
     """
     if initializer:
         value = initializer
@@ -27,7 +37,7 @@ def reduce(it, initializer=None):
 
 
 def expr_to_list(expr):
-    """replace operators with their corresponding functions
+    """Replace operators with their corresponding functions
     """
     return [
         ops[elem] if elem in ('+', '*') else int(elem)
@@ -35,22 +45,35 @@ def expr_to_list(expr):
     ]
 
 
+def proxy_pred(input):
+    """Proxy for precedence computation
+    """
+    if precedence_op:
+        while pmatch := precedence_op.search(input):
+            found = pmatch.group(1)
+            input = input.replace(
+                found,
+                str(list(reduce(iter(expr_to_list(found))))[0])
+            )
+    return str(list(reduce(iter(expr_to_list(input))))[0])
+
+
 def deep_compute(input):
-    """Compute all sub groups of parenthesis, recursively
+    """Compute all sub groups of parenthesis
     """
     while match := pattern.search(input):
         input = input.replace(
             match.group(0),
-            str(list(reduce(iter(expr_to_list(match.group(1)))))[0])
+            proxy_pred(match.group(1))
         )
+
     return input
 
 
 with open('day18.input') as f:
     inputs = f.readlines()
 
-
 print(sum(
-    next(reduce(iter(expr_to_list(deep_compute(input)))))
+    int(proxy_pred(deep_compute(input)))
     for input in inputs
 ))
